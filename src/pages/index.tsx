4ec6a0130db3da130e2a34cbd18dcb5914bf8357
Plínio Netto <plinio.ice.pns@gmail.com>
2022-03-06 14:38:35 -0300

    feat: implementa a renderização da tabela de clientes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
+import Table from '../components/Table'
+import Client from '../core/Client'
 
 export default function Home() {
+  const clients = [
+    new Client('Ana', 34, '1'),
+    new Client('Bia', 45, '2'),
+    new Client('Carlos', 23, '3'),
+    new Client('Pedro', 54, '4')
+  ]
   return (
     <div className={`
       flex justify-center h-screen items-center
@@ -10,7 +18,7 @@ export default function Home() {
       text-white
     `}>
       <Layout title="Cadastro Simples">
-        <span>Content</span>  
+        <Table clients={clients}></Table> 
       </Layout>
     </div>
   )
