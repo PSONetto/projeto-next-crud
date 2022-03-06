92d5073b51e9929d0ec2ad7df7c1c9f1f64f92d5
Plínio Netto <plinio.ice.pns@gmail.com>
2022-03-06 01:48:02 -0300

    COMMIT ALL THE FILES!

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
