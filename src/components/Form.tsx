65a2e176109b8ff0fc302eaf54f55f1c4928e555
Plínio Netto <plinio.ice.pns@gmail.com>
2022-03-06 22:56:32 -0300

    feat: implementa a funcionalidade de alternar entre tabela e formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,6 +5,8 @@ import Button from './Button'
 
 interface FormProps {
     client: Client
+    clientChanged?: (client: Client) => void
+    canceled?: () => void
 }
 
 export default function Form(props: FormProps) {
@@ -35,10 +37,11 @@ export default function Form(props: FormProps) {
                 onChangeValue={setAge}
             />
             <div className=' flex justify-end mt-7'>
-                <Button color="blue" className='mr-2'>
+                <Button color="blue" className='mr-2' 
+                    onClick={() => props.clientChanged?.(new Client(name, +age, id))}>
                     {id ? 'Alterar' : 'Salvar'}
                 </Button>
-                <Button>
+                <Button onClick={props.canceled}>
                     Cancelar
                 </Button>
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Table from '../components/Table'
 import Client from '../core/Client'
 import Button from '../components/Button'
 import Form from '../components/Form'
+import { useState } from 'react'
 
 export default function Home() {
   const clients = [
@@ -20,6 +21,12 @@ export default function Home() {
     console.log(`Deletando ${client.name}`)
   }
 
+  function saveClient(client: Client) {
+    console.log(client)
+  }
+
+  const [visible, setVisible] = useState<'table' | 'form'>('table')
+
   return (
     <div className={`
       flex justify-center h-screen items-center
@@ -27,14 +34,26 @@ export default function Home() {
       text-white
     `}>
       <Layout title="Cadastro Simples">
-        <div className='flex justify-end'>
-          <Button color='green' className='mb-4'>Novo Cliente</Button>
-        </div>
-        <Table clients={clients}
-          selectedClient={selectedClient}
-          deletedClient={deletedClient}
-        />
-        <Form client={clients[0]}/>
+        {visible === 'table' ? (
+          <>
+            <div className='flex justify-end'>
+              <Button color='green' className='mb-4'
+                onClick={() => setVisible('form')}>
+                Novo Cliente
+              </Button>
+            </div>
+            <Table clients={clients}
+              selectedClient={selectedClient}
+              deletedClient={deletedClient}
+            />
+          </>
+        ) : (
+          <Form
+            client={clients[0]}
+            clientChanged={saveClient}
+            canceled={() => setVisible('table')}
+          />
+        )}
       </Layout>
     </div>
   )
